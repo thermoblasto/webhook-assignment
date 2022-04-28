978d80e1b6aaa03bbbfc60fac1fe90219bb93de7
Ankur Goyal <thermoblasto@gmail.com>
2022-04-29 01:22:04 +0300

    Initial working code

--- app.py
+++ app.py
@@ -1,63 +1,33 @@
+from flask import Flask, request, Response,json
 import mysql.connector
-import json
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, Docker!'
+  
+@app.route('/webhook', methods=['POST'])
 
-@app.route('/widgets')
-def get_widgets():
-  mydb = mysql.connector.connect(
-    host="mysqldb",
-    user="root",
-    password="p@ssw0rd1",
-    database="inventory"
-  )
-  cursor = mydb.cursor()
+def respond():
+    data = request.json
+    idfv=data["idfv"]
+    device_category=data["device_category"]
+    advertising_id=data["advertising_id"]
+    android_id=data["android_id"]
+    af_sub1=data["af_sub1"]
+    customer_user_id=data["customer_user_id"]
+    is_lat=data["is_lat"]
 
-
-  cursor.execute("SELECT * FROM widgets")
-
-  row_headers=[x[0] for x in cursor.description] #this will extract row headers
-
-  results = cursor.fetchall()
-  json_data=[]
-  for result in results:
-    json_data.append(dict(zip(row_headers,result)))
-
-  cursor.close()
-
-  return json.dumps(json_data)
-
-@app.route('/initdb')
-def db_init():
-  mydb = mysql.connector.connect(
-    host="mysqldb",
-    user="root",
-    password="p@ssw0rd1"
-  )
-  cursor = mydb.cursor()
-
-  cursor.execute("DROP DATABASE IF EXISTS inventory")
-  cursor.execute("CREATE DATABASE inventory")
-  cursor.close()
-
-  mydb = mysql.connector.connect(
-    host="mysqldb",
-    user="root",
-    password="p@ssw0rd1",
-    database="inventory"
-  )
-  cursor = mydb.cursor()
-
-  cursor.execute("DROP TABLE IF EXISTS widgets")
-  cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-  cursor.close()
-
-  return 'init database'
-
-if __name__ == "__main__":
-  app.run(host ='0.0.0.0')
+    mydb = mysql.connector.connect(
+       host="mysqldb",
+       user="root",
+       password="p@ssw0rd1",
+       database="inventory"
+     )
+    cursor = mydb.cursor()
+    cursor.execute(''' INSERT INTO attribution (idfv,device_category,af_sub1,customer_user_id,is_lat) VALUES(%s,%s,%s,%s,%s)''',(idfv,device_category,af_sub1,customer_user_id,is_lat))
+    cursor.execute(''' INSERT INTO attribution_identifier (advertising_id,android_id) VALUES(%s,%s)''',(advertising_id,android_id))
+    mydb.commit()
+    cursor.close()
+    return Response(status=200)
